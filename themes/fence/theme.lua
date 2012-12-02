de25ac0094f74f1e26b600eb7585875c9d7f4536
Thomas Adam <thomas@xteddy.org>
2012-12-02 23:25:10 +0000

    Initial import.

---
+++ themes/fence/theme.lua
@@ -0,0 +1,85 @@
+-- fence, awesome3 theme, by jorick
+
+--{{{ Main
+require("awful.util")
+
+theme = {}
+
+home          = os.getenv("HOME")
+config        = awful.util.getdir("config")
+shared        = "/usr/local/share/awesome"
+if not awful.util.file_readable(shared .. "/icons/awesome16.png") then
+    shared    = "/usr/local/share/awesome"
+end
+sharedicons   = shared .. "/icons"
+sharedthemes  = shared .. "/themes"
+themes        = config .. "/themes"
+themename     = "/fence"
+if not awful.util.file_readable(themes .. themename .. "/theme.lua") then
+       themes = sharedthemes
+end
+themedir      = home .. "/.config/awesome/themes/" .. themename
+
+if awful.util.file_readable(config .. "/vain/init.lua") then
+    theme.useless_gap_width  = "2"
+end
+--}}}
+
+theme.font          = "fixed 10"
+
+theme.bg_normal     = "#368DFF"
+theme.bg_focus      = "#7366FF"
+theme.bg_urgent     = "#EE36C6"
+theme.bg_minimize   = "#C8C8C8"
+
+theme.fg_normal     = "#d0d0cc"
+theme.fg_focus      = "#d6d9ba"
+theme.fg_urgent     = "#2c2c2c"
+theme.fg_minimize   = "#2c2c2c"
+
+theme.border_width  = "3"
+theme.border_normal = "darkgreen"
+theme.border_focus  = "darkorange"
+theme.border_marked = "#07AD08"
+
+theme.taglist_squares_sel   = themedir .. "/taglist/squarefw.png"
+theme.taglist_squares_unsel = themedir .. "/taglist/squarew.png"
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- [taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+-- Display the taglist squares
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Define the image to load
+-- You can use your own layout icons like this:
+theme.layout_fairh = themedir .. "/layouts/fairhw.png"
+theme.layout_fairv = themedir .. "/layouts/fairvw.png"
+theme.layout_floating  = themedir .. "/layouts/floatingw.png"
+theme.layout_magnifier = themedir .. "/layouts/magnifierw.png"
+theme.layout_max = themedir .. "/layouts/maxw.png"
+theme.layout_fullscreen = themedir .. "/layouts/fullscreenw.png"
+theme.layout_tilebottom = themedir .. "/layouts/tilebottomw.png"
+theme.layout_tileleft   = themedir .. "/layouts/tileleftw.png"
+theme.layout_tile = themedir .. "/layouts/tilew.png"
+theme.layout_tiletop = themedir .. "/layouts/tiletopw.png"
+theme.layout_spiral  = themedir .. "/layouts/spiralw.png"
+theme.layout_dwindle = themedir .. "/layouts/dwindlew.png"
+
+return theme
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
